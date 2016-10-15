e92105b6abbd9c15734285c85d2e96b60983d920
Zac Lee <zaclee@Zac-ZCW-29.local>
2016-10-15 12:39:33 -0400

    working project here

---
+++ www/js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('app.controllers', [])
+  
+.controller('teamsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('scheduleCtrl', ['$scope', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $http) {
+$scope.games = [];
+$http.get('/jsonHard.json')
+// http://api.football-data.org/v1/teams/57/fixtures
+.then(function (response) {
+	console.log(response);
+	$scope.games = response.data;
+})
+
+
+}])
+      
+.controller('pageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,49 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.teams', {
+    url: '/Teams',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/teams.html',
+        controller: 'teamsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.schedule', {
+    url: '/Schedule',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/schedule.html',
+        controller: 'scheduleCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('page', {
+    url: '/page4',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1/Teams')
+
+  
+
+});
