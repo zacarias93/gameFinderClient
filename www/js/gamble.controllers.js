9d90a0369a1b46cef9707498190060281c5054ad
Zac <zeelee93@gmail.com>
2016-11-06 04:14:11 -0500

    gamble feature - hard coded team for gameOfWeek

--- www/js/gamble.controllers.js
+++ www/js/gamble.controllers.js
@@ -9,42 +9,62 @@
     function gambleController(userService, gameService, $q) {
 
         var gambleVm = this;
-        var games = [];
-        var thirdRankTeam = getThirdRankTeam();
+        var thirdRankTeam = '';
+        var team = '';
 
         gambleVm.league = userService.getUser().league
         gambleVm.matchOfTheWeek = {};
 
 
 //initialize team values then run your function w/ a button! COME BACK TO THIS
-        function getMatchOfTheWeek() {
+
+        gambleVm.getMatchOfTheWeek = function() { 
+            var games = [];
+            var teamname = team;
+            console.log('teamname (matchOfTheWeek) : ' , teamname);
             console.log(gambleVm.league);
             gameService.getGames(gambleVm.league).then(function(response) {
+
                 games = response.fixtures;
+                var length = games.length;
+                console.log(length);
                 console.log(games);
+                console.log(thirdRankTeam);
 
-
-                // for(var i=0; i<380; i++) {
-                //     if(games[i].status == 'SCHEDULED') {
-                //         if(games[i].awayTeamName == teamname || games[i].homeTeamName == teamname) {
-                //             gambleVm.matchOfTheWeek = games[i];
-                //             console.log(gambleVm.matchOfTheWeek);
-                //         }
-                //     }
-                // }
+                for(var i=0; i<length; i++) {
+                    if(games[i].status == 'SCHEDULED') {
+                        if(games[i].awayTeamName == teamname || games[i].homeTeamName == teamname) {
+                            gambleVm.matchOfTheWeek = games[i];
+                            console.log(gambleVm.matchOfTheWeek);
+                            break;
+                        }
+                    }
+                }
             })
+        }
+//Setting an arbitrary team each week for the 'match of the week' - find better way to do this later...
+        function setTeamName() {
+
+            switch (gambleVm.league) {
+                case 'English Premier League': team = 'Arsenal FC';
+                    break;
+                case 'Bundesliga': team = 'FC Bayern München';
+                    break;
+                case 'Primera Division' : team = 'FC Barcelona';
+                    break;
+                default: team = 'Something wrong happened in switch statement in gamble.controllers.js';
+            }
+            console.log('setTeamName() : ' , team);
         }
 
         function getThirdRankTeam() {
             gameService.getStandings(gambleVm.league).then(function(response) {
                 var data = response.standing;
                 thirdRankTeam = data[2].teamName;
-                console.log(thirdRankTeam);
+                console.log('thirdRankTeam : ', thirdRankTeam);
                 return thirdRankTeam;
             })
         }
-
-
 
 
         activate();
@@ -52,12 +72,9 @@
         ////////////////
 
         function activate() {
-            gameService.getStandings(gambleVm.league).then(function(response) {
-                var data = response.standing;
-                thirdRankTeam = data[2].teamName;
-                console.log(thirdRankTeam);
-                return thirdRankTeam;
-            })
+            getThirdRankTeam();
+            setTeamName();
+            gambleVm.getMatchOfTheWeek();
         }
         
 
